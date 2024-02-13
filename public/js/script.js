b464c531734649ded06139d4011700bb8cdbd981
codeyumm <pvashi006@gmail.com>
2024-02-12 21:02:40 -0500

    Made design responsive for mobile and tablet

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
 console.log("hello from script.js");
+
+window.onload = function() {
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector("#nav-menu");
+    
+    console.log(hamburger)
+    
+    hamburger.addEventListener('click', () => {
+    
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    
+    
+    } )
+
+}
